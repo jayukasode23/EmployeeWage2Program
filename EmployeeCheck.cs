b5b839ae67d50aa0b4d0de48b92fd6f333826057
jayukasode23 <jayukasode2398@gmail.com>
2022-10-06 19:59:55 +0530

    Added Employee Attendance

---
+++ EmployeeCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProblem2
+{
+    internal class EmployeeCheck
+    {
+        public void CheckAttendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+
+            }
+        }
+    }
+}
